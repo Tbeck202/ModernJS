2c7781657b08f00dd0fe4d1301061d8e14993228
Tyler Beck <tbeck202@gmail.com>
2019-12-30 17:20:51 -0800

    commit ternary operator review

---
+++ ternaryOperators/ternaryOperators.js
@@ -0,0 +1,20 @@
+console.log("connected!");
+
+//EX1----------------------------------------------------
+let num = 7;
+if(num === 7){
+    console.log("LUCKY!!!");
+}
+else {
+    console.log("BAD!!!");
+}
+
+num === 7 ? console.log("LUCKY!!!!") : console.log("BAD!!!!");
+
+//EX2----------------------------------------------------
+let status = 'offline';
+
+                    //condition  ?  if true : if false
+let color = status === 'offline' ? 'red' : 'green';
+console.log(color);
+
