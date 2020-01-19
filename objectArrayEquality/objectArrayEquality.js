f3871e4cc1f95dcb9a67f1ffaeb199648f61d2cc
Tyler Beck <tbeck202@gmail.com>
2020-01-19 11:51:54 -0800

    Commit JS array/object practice

---
+++ objectArrayEquality/objectArrayEquality.js
@@ -0,0 +1,13 @@
+console.log('Connected!');
+let nums = [ 1, 2, 3 ];
+let mystery = [ 1, 2, 3 ];
+let moreNums = nums;
+
+nums === mystery; //False
+nums === moreNums; //True
+
+const user = {
+	username: 'CherryGarcia8',
+	email: 'garcia@gmail.com',
+	notifications: []
+};
