4711ec064c0d33b3c25034fdf16ffa7157027848
Tyler Beck <tbeck202@gmail.com>
2020-01-23 19:04:07 -0800

    Update functionPractice.js
    completed get card function

--- functionPractice/passwordValidator/functionPractice.js
+++ functionPractice/passwordValidator/functionPractice.js
@@ -33,3 +33,15 @@ function isPangram(sentence) {
 	}
 	return true;
 }
+
+function pick(arr) {
+	const idx = Math.floor(Math.random() * arr.length);
+	return arr[idx];
+}
+
+function getCard() {
+	const values = [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ];
+	const suits = [ 'clubs', 'spades', 'hearts', 'diamonds' ];
+
+	return { value: pick(values), suit: pick(suits) };
+}
