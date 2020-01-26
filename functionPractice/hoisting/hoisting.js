cb0c59897b3537d95a99f1e39ac0d7f5e882ad9d
Tyler Beck <tbeck202@gmail.com>
2020-01-26 10:22:39 -0800

    Commit hoisting practice

---
+++ functionPractice/hoisting/hoisting.js
@@ -0,0 +1,17 @@
+console.log('Connected!');
+
+// console.log(animal);
+// var animal = 'Tapir';
+
+// console.log(shrimp);
+// let shrimp = 'Harlequin Shrimp';
+
+howl();
+function howl() {
+	console.log('Awoooooooo!');
+}
+
+hoot();
+var hoot = function() {
+	console.log('HOO HOO');
+};
