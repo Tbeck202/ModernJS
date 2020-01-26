91eae9823c311b2b3c63678c9008e87f51670118
Tyler Beck <tbeck202@gmail.com>
2020-01-25 17:28:12 -0800

    Commit call back practice

---
+++ functionPractice/callbacks/callbacks.js
@@ -0,0 +1,14 @@
+console.log('Connected!');
+
+// function grumpus() {
+// 	alert("GO AWAY!!!");
+// }
+
+// setTimeout(function() {
+// 	alert('Welcome!');
+// }, 500);
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', function() {
+	alert('I TOLD YOU NOT TO CLICK ME!!');
+});
