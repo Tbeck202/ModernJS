6752d1e7e9c4c32a6e8da98755d26f0ae4d314a3
Tyler Beck <tbeck202@gmail.com>
2020-03-24 18:16:14 -0700

    working with the DOM

---
+++ theDOM/intro/app.js
@@ -0,0 +1,12 @@
+const allLis = document.querySelectorAll('li');
+const colors = [ 'red', 'orange', 'yellow', 'green', 'blue', 'purple' ];
+// const btn = document.querySelectorAll('button');
+// const todo = document.querySelectorAll('.todo');
+// for (let i = 0; i < allLis.length; i++) {
+// 	allLis[i].innerText = 'Go Jazz!';
+// }
+
+// allLis.forEach((el, idx) => {
+// 	const color = colors[idx];
+// 	el.style.color = color;
+// });
