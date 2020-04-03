749e81d679956d9124a730d2580c94cae0b20d67
Tyler Beck <tbeck202@gmail.com>
2020-04-03 12:24:26 -0700

    Promises, then, catch, chaining

--- callStack/promises/app.js
+++ callStack/promises/app.js
@@ -1,63 +1,31 @@
-// const makeDogProm = () => {
-// 	return new Promise((resolve, reject) => {
-// 		setTimeout(() => {
-// 			const rand = Math.random();
-// 			if (rand < 0.5) {
-// 				resolve();
-// 			} else {
-// 				reject();
-// 			}
-// 		}, 5000);
-// 	});
-// };
+const btn = document.querySelector('button');
 
-// makeDogProm()
-// 	.then(() => {
-// 		console.log('Yay we got a dog!');
-// 	})
-// 	.catch(() => {
-// 		console.log(':( No dog');
-// 	});
-
-const fakeRequest = (url) => {
+const moveX = (element, amount, delay) => {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
-			const pages = {
-				'/users': [ { id: 1, username: 'Bilbo' }, { id: 5, username: 'Esmerelda' } ],
-				'/about': 'This is the about page!'
-			};
-			const data = pages[url];
-			if (data) {
-				resolve({ status: 200, data });
+			const bodyBoundary = document.body.clientWidth;
+			const elRight = element.getBoundingClientRect().right;
+			const currLeft = element.getBoundingClientRect().left;
+			if (elRight + amount > bodyBoundary) {
+				reject({ bodyBoundary, elRight, amount });
 			} else {
-				reject({ status: 404 });
+				element.style.transform = `translateX(${currLeft + amount}px)`;
+				resolve();
 			}
-		}, 1000);
+		}, delay);
 	});
 };
 
-fakeRequest('/about')
-	.then((res) => {
-		console.log('Status Code:', res.status);
-		console.log('Data:', res.data);
-		console.log('Request worked!');
-	})
-	.catch((res) => {
-		console.log(res.status);
-		console.log('Request failed:(');
+moveX(btn, 300, 1000)
+	.then(() => moveX(btn, 300, 1000))
+	.then(() => moveX(btn, 300, 1000))
+	.then(() => moveX(btn, 300, 1000))
+	.then(() => moveX(btn, 300, 1000))
+	.then(() => moveX(btn, 300, 1000))
+	.catch(({ bodyBoundary, elRight }) => {
+		console.log(`Body is only ${bodyBoundary}px wide.`);
+		console.log(`But you tried to move to ${bodyBoundary + elRight}px`);
 	});
-fakeRequest('/dogs')
-	.then((res) => {
-		console.log('Status Code:', res.status);
-		console.log('Data:', res.data);
-		console.log('Request worked!');
-	})
-	.catch((res) => {
-		console.log(res.status);
-		console.log('Request failed:(');
-	});
-
-// const btn = document.querySelector('button');
 
 // const moveX = (element, amount, delay, onSuccess, onFail) => {
 // 	setTimeout(() => {
